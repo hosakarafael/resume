2954a679e37fcd9b7c0cb867595db87f82ea5c00
Rafael Hosaka <rafaelhosaka@gmail.com>
2022-07-09 15:29:21 -0300

    -remove dev config from cookiecofig

--- src/utils/cookieConfig.ts
+++ src/utils/cookieConfig.ts
@@ -1,13 +1,5 @@
 import { CookieSerializeOptions } from "cookie";
 
-export const devCookieConfig = {
-  httpOnly: true,
-  secure: false,
-  sameSite: "strict",
-  maxAge: 3600,
-  path: "/",
-} as CookieSerializeOptions;
-
 export const cookieConfig = {
   httpOnly: true,
   secure: true,
